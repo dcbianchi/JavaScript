9cdbe1f8341a25d52e09dc50459d142ae90ca704
André Bianchi <dcbianchi15@gmail.com>
2023-03-29 14:21:10 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if (vel > 60 ) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. Multado`)
+}
+console.log(`cuidado`)
