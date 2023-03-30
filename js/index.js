fb4cbcdbba5dd977f3e6cdf0c3ce93e4d0d91128
André Bianchi <dcbianchi15@gmail.com>
2023-03-30 15:16:06 -0300

    MDe12

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
-//A11e1
-var agora = new Date() //instanciando o valor de retorno da função Date() na variavel agora.
-var hora = agora.getHours()//recuperando hora local do sistema (getHours()) da instancia agora para a variavel hora
+ //Exercicios do Modulo D
+ function carregar(){
+    var agora = new Date() //Instanciando Data na Variavel Agora
+    var hora = agora.getHours() //capturando a hora do sistema pelo retorno da função getHours do objeto Agora
+    var msg = document.getElementById('pHora')
+    var foto = document.getElementById('imgCenterPage')
 
-
-//O ID do paragrafo é time
-//O ID da imagem é imgCenterPage
-
-if (hora < 12) {
-    time.querySelector(time) = `Agora são ${hora}`
-    .querySelector(imgCenterPage)
-} else if (hora < 18) {
-
-} else (hora < 23) {
-
-}
+    //Usando a variavel hora como base, condicionamos o Paragrafo time
+    if (hora >=0 && hora < 12) {
+        //Bom dia
+        msg.innerHTML = `Bom dia, agora são ${hora} hs`
+        foto.src = `img/sunrise.jpg`
+        document.body.style.background = '#e2cd2f'
+    } else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML = `Boa tarde, agora são ${hora} hs`
+        foto.src = `img/sunset.jpg`
+        document.body.style.background = '#e2cd2f'
+    } else {
+        msg.innerHTML = `Boa noite, agora são ${hora} hs`
+        foto.src = `img/night.jpg`
+    }
+ }
