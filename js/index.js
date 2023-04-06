b279c820162ea1a6b814d3a4007b84e602e82ef2
André Bianchi <dcbianchi15@gmail.com>
2023-04-06 12:50:54 -0300

    MEa14e5
    Super contador

--- js/index.js
+++ js/index.js
@@ -2,27 +2,39 @@
 // Professor Gustavo Guanabara
 // Tema do modulo: Repetições em JavaScript
 
-// Exercici 16 - Contagem
+// Exercici 16 - Super Contador
+function contar(){ 
+    // Capturando os campos HTML 
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pass = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
+    //Checagem de dados.
+    if (ini.value.length==0 || fim.value.length==0 || pass.value.length==0){
+        window.alert('[Erro] Faltam dados')
+        } 
+        else {
+            res.innerHTML = 'Contando: ' //Alterando o conteudo da Div
+            let i = Number(ini.value)  // Convertendo os tipos das variaveis em numeros
+            let f = Number(fim.value)
+            let p = Number(pass.value)
 
-
-function contar(){
-    var ini = document.getElementById('sta')
-    var fim = document.getElementById('end')
-    var pass = document.getElementById('pass')
-    var contage = document.getElementById('contagem')
-    var txt = document.getElementById('txt')
-    if (ini == 0 || fim ==0 || pass == 0) {
-        txt.innerHTML = 'impossivel contar, verifique os dados'
-    } else if (inicio < fim) {
-        var c = pass
-        for(c; c >= fim; c++) {
-            console.log(`${c}`)
-        }
-    } else if (fim > inicio) {
-        var c = pass
-        for (c; fim >= c; c--){
-            console.log(`${c}`)
-        }
+            // Contagem crescente
+            if (i < f) {
+                for(let c = i; c <= f; c += p){
+                    res.innerHTML += ` ${c} \u{1F449}`
+                }
+            }
+            else {
+                // Contagem regressiva
+                for (let c = i; c>= f; c -= p){
+                    res.innerHTML += ` ${c} \u{1f449}`
+                }
+            }
     }
+    //chequered flag emoji
+    res.innerHTML += `\u{1f3c1}`
 }
+
+// Exercicio 17 
